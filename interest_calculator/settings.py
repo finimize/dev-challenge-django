67d85b15de5489c92b9300261d8c3634cc32a831
Florian Moga <moga.flo@gmail.com>
2018-02-08 12:27:27 +0200

    Tweaks

--- interest_calculator/settings.py
+++ interest_calculator/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -68,17 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'interest_calculator.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
