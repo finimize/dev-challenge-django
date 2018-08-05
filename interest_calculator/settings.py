c90b1e7b0e9346624193b3a1a247203eeba32ea2
YOURNAME <YOUREMAIL>
2018-08-05 13:09:23 +0100

    add calculate functionality and tests

--- interest_calculator/settings.py
+++ interest_calculator/settings.py
@@ -106,3 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
+}
